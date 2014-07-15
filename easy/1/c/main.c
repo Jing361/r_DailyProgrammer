fa98c0fe25c3f019503c60989a6a340062c5086c
Chris Samuelson <chris.sam55@gmail.com>
2014-07-15 15:42:58 -0400

    Fixed input errors.

--- easy/1/c/main.c
+++ easy/1/c/main.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
 
+//TODO: Add checks for input types (numbers/letters etc)
 int main(int argc, char **argv){
     char name[255];
-    int age;
+    char age[5];
     char username[255];
     FILE *fp;
     fp = fopen("record", "wt");
 
     printf("Name: ");
     scanf("%s", name);
+
     printf("Age: ");
-    scanf("%d", &age);
+    scanf("%s", age);
+
     printf("Username: ");
     scanf("%s", username);
-    printf("Your name is %s, you are %d years old, your username is %s.\n", name, age, username);
-    fprintf(fp, "Name: %s\nAge: %d\nUsername: %s\n", name, age, username);
+
+    printf("Your name is %s, you are %s years old, and your username is %s.\n", name, age, username);
+    fprintf(fp, "Name: %s\nAge: %s\nUsername: %s\n", name, age, username);
     fclose(fp);
 
     return 0;
 }
+
