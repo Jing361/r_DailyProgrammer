39a8768898843a01fc9fa3389d5be43f65356e7f
Chris Samuelson <chris.sam55@gmail.com>
2014-07-16 16:37:34 -0400

    Take command line parameter for max value.

--- hard/1/c/main.c
+++ hard/1/c/main.c
@@ -1,19 +1,39 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<errno.h>
+#include<limits.h>
 
-int eval(int, int);
+int eval(int high, int low);
 
 int main(int argc, char **argv){
-  printf("Choose a number between 1 and 100 inclusively.\n");
-  printf("Press enter to continue.");
-  getchar();
   char resp = 'n';
   int high = 100;
   int low = 1;
   int guess;
-  //TODO: let it terminate!! wtfff
+
+  if(argc == 2){
+    char *endptr;
+    long val;
+    errno = 0;
+
+    val = strtol(argv[1], &endptr, 10);
+    if((errno == ERANGE && (val == LONG_MAX || val == LONG_MIN)) || (errno != 0 && val == 0)) {
+      printf("Error parsing number parameter\n");
+    } else if(endptr == argv[1]) {
+      printf("No digits found, ignoring program parameter\n");
+    } else {
+      // If we got here, strtol() successfully parsed a number.
+      high = val;
+    }
+  }
+
+  printf("Choose a number between 1 and %d inclusively.\n", high);
+  printf("Press enter to continue.");
+  getchar();
+
   while(resp != 'y' && resp != 'Y'){
     guess = eval(high, low);
-    printf("Is %d your number?\n(y or Y for correct numberh or H for too high and l or L for too low)\n", guess);
+    printf("Is %d your number?\n(Y for correct number H for too high and L for too low)\n", guess);
     scanf(" %c", &resp);
     if(resp == 'h' || resp == 'H'){
       high = guess;
