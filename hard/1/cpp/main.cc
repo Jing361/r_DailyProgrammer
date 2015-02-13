5b8464ed98bcffc65107095adbf76c97818c8b6a
Chris Samuelson <chris.sam55@gmail.com>
2015-02-12 20:00:43 -0500

    Nearly complete number guessing.

--- hard/1/cpp/main.cc
+++ hard/1/cpp/main.cc
@@ -1,56 +1,81 @@
 #include<iostream>
-#include<stdio.h>
-#include<stdlib.h>
-#include<errno.h>
-#include<limits.h>
+#include<cstdlib>
+#include<string>
+#include<sstream>
+#include<cmath>
+
 using std::cout;
 using std::cin;
 using std::endl;
-
-int eval(int high, int low);
+using std::string;
+using std::stringstream;
 
 int main(int argc, char** argv){
-  char resp = 'n';
-  int high = 100;
-  int low = 1;
-  int guess;
+  int max = 100;
+  int min = 0;
+  int val = 0;
+  int upperBound = max;
+  int lowerBound = min;
+  char test[256];
+  string input;
 
-  if(argc == 2){
-    char *endptr;
-    long val;
-    errno = 0;
+  if(argc > 3){
+    cout << "Incorrect number of arguments." << endl;
+    return 1;
+  }else if(argc == 2){
+    max = atoi(argv[1]);
+  }else if(argc == 3){
+    min = atoi(argv[1]);
+    max = atoi(argv[2]);
+  }
 
-    val = strtol(argv[1], &endptr, 10);
-    if((errno == ERANGE && (val == LONG_MAX || val == LONG_MIN)) || (errno != 0 && val == 0)) {
-      cout << "Error parsing number parameter" << endl;
-    } else if(endptr == argv[1]) {
-      cout << "No digits found, ignoring program parameter";
-    } else {
-      // If we got here, strtol() successfully parsed a number.
-      high = val;
+  stringstream inputS;
+  cout << endl;
+  cout << "You need to choose a number between " << min << " and " << max << "." << endl;
+  cout << "I will guess it in about " << log2(max - min) << " guesses." << endl;
+  cout << "Keep it in your mind." << endl;
+  do{
+    cout << "I will try to guess it; are you ready?[y/n]" << endl;
+    cin.getline(test, 255);
+    input = test;
+  }while(input.length() > 1 || (input[0] != 'y' && input[0] != 'n'));
+
+  if(input[0] == 'n'){
+    cout << "O, ok..Bye!" << endl;
+    return 0;
+  }
+
+  while(true){
+    do{
+      cout << "Ok, is your number above, below, or equal to:\t" << ((upperBound - lowerBound)/2) << "?[a/b/e]" << endl;
+      cin.getline(test, 255);
+      input = test;
+    }while(input[0] != 'a' && input[0] != 'b' && input[0] != 'e');
+
+    if(input[0] == 'a'){
+      lowerBound = (upperBound - lowerBound) / 2;
+    }else if(input[0] == 'b'){
+      upperBound = (upperBound - lowerBound) / 2;
+    }else if(input[0] == 'e'){
+      cout << "AHA!! I win. Your number was " << ((upperBound - lowerBound) / 2) << "." << endl;
+      cout << "I told you I could get it." << endl;
+      cout << "Better luck next time! :)" << endl;
+      return 0;
+    }else if(upperBound == lowerBound){
+      cout << "What the hell?" << endl;
+      cout << "How'd you do that?" << endl;
+      cout << "O well, you win. :(" << endl;
+      return 1;
     }
   }
 
-  cout << "Choose a number between 1 and " << high << " inclusively." << endl;
-  cout << "Press enter to continue.";
-  cin.ignore();
+  do{
+    cin.getline(test, 256);
+    input = test;
+    inputS.str(input);
+  }while(!(inputS >> val));
+  cout << "Ok, go!" << endl;
 
-  while(resp != 'y' && resp != 'Y'){
-    guess = eval(high, low);
-    cout << "Is " << guess << " your number?" << endl;
-    cout << "(Y for correct number H for too high and L for too low)" << endl;
-    cin >> resp;
-
-    if(resp == 'h' || resp == 'H'){
-      high = guess;
-    } else if(resp == 'l' || resp == 'L'){
-      low = guess;
-    }
-  }
   return 0;
-}
-
-int eval(int high, int low){
-  return ((high - low)/2) + low;
 }
 
