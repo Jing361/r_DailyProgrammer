cdc8a3d183c39547384514818a41f5f81fe0423e
Chris Samuelson <chris.sam55@gmail.com>
2015-04-21 20:34:28 -0400

    Finished algorithm to find anagrams.

--- inter/5/main.cc
+++ inter/5/main.cc
@@ -20,6 +20,16 @@ int main(){
     std::sort((*it).begin(), (*it).end());
   }
 
+  for(unsigned int i = 0; i < sorted.size(); ++i){
+    std::string tmp(sorted[i]);
+    std::cout << words[i] << std::endl;
+    for(unsigned int j = i + 1; j < sorted.size(); ++j){
+      if(tmp == sorted[j]){
+        std::cout << "\t" << words[j] << std::endl;
+      }
+    }
+  }
+
   dictFile.close();
   return 0;
 }
