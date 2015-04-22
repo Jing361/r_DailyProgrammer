3e3c7296e570ce706463dfaa5860840af061f951
Chris Samuelson <chris.sam55@gmail.com>
2015-04-21 23:06:44 -0400

    Added dialogue to request anagrams for a word.

--- inter/5/main.cc
+++ inter/5/main.cc
@@ -2,17 +2,23 @@
 #include<vector>
 #include<iostream>
 #include<algorithm>
+#include<map>
 
 int main(){
   std::ifstream dictFile("dictionary", std::ifstream::in);
   std::vector<std::string> words;
   std::vector<std::string> sorted;
+  std::map<std::string, std::vector<std::string> > anagrams;
   char cword[128];
+
+  std::cout << "Processing dictionary...\n" << std::flush;
 
   while(!dictFile.eof()){
     dictFile.getline(cword, 127);
     words.push_back(std::string(cword));
   }
+
+  std::cout << "\tDictionary read.\n" << std::flush;
 
   sorted = words;
 
@@ -20,13 +26,25 @@ int main(){
     std::sort((*it).begin(), (*it).end());
   }
 
+  std::cout << "\tWords processed.\n" << std::flush;
+
   for(unsigned int i = 0; i < sorted.size(); ++i){
     std::string tmp(sorted[i]);
-    std::cout << words[i] << std::endl;
-    for(unsigned int j = i + 1; j < sorted.size(); ++j){
+    for(unsigned int j = 0; j < sorted.size(); ++j){
       if(tmp == sorted[j]){
-        std::cout << "\t" << words[j] << std::endl;
+        anagrams[words[i]].push_back(words[j]);
       }
+    }
+  }
+
+  std::cout << "Dictionary processed!\n" << std::flush;
+
+  while(true){
+    std::string input;
+    std::cout << "I'd like to see anagrams for ";
+    std::cin >> input;
+    for(auto it = anagrams[input].begin(); it != anagrams[input].end(); ++it){
+      std::cout << "\t" << *it << std::endl;
     }
   }
 
