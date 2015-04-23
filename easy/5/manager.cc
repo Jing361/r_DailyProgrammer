5f87d9c0bdc89453ae10ca1e41a0658ada612048
Chris Samuelson <chris.sam55@gmail.com>
2015-04-23 16:58:03 +0000

    Manager now uses better input practices.

--- easy/5/manager.cc
+++ easy/5/manager.cc
@@ -12,26 +12,28 @@ int main(){
     std::cout << std::get<1>(std::get<1>(*it)) << std::endl;
     std::cout << std::endl;
   }
+
+  int opt;
   while(true){
-    int opt;
+    while(std::cout << "What would you like to do?\t\n"
+                    << "1.\tAdd a user\n"
+                    << "2.\tRemove a user\n"
+                    << "3.\tFinish\n" &&
+        (!(std::cin >> opt) || opt < 1 || opt > 3)){
+      std::cout << "Invalid input\n";
+    }
     std::string name;
     std::string pass;
 
-    std::cout << "What would you like to do?\t" << std::endl;
-    std::cout << "1.\tAdd a user" << std::endl;
-    std::cout << "2.\tRemove a user" << std::endl;
-    std::cout << "3.\tFinish" << std::endl;
-    std::cin >> opt;
-    if(std::cin.fail()){
-      continue;
+    if(opt < 3){
+      std::cout << "Please enter username and password for user:" << std::endl;
+      std::cout << "username:\t";
+      std::cin >> name;
+      std::cout << "password:\t";
+      auth.setStdinEcho(false);
+      std::cin >> pass;
+      auth.setStdinEcho(true);
     }
-    std::cout << "Please enter username and password for user:" << std::endl;
-    std::cout << "username:\t";
-    std::cin >> name;
-    std::cout << "password:\t";
-    auth.setStdinEcho(false);
-    std::cin >> pass;
-    auth.setStdinEcho(true);
     if(opt == 1){
       auth.mkUser(name, pass);
     } else if(opt == 2){
