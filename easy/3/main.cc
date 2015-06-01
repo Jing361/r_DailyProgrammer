723612e31c471bf981d42599e5d206bdc9f1bf6f
Chris Samuelson <chris.sam55@gmail.com>
2015-06-01 19:36:13 -0400

    Added some help messages.

--- easy/3/main.cc
+++ easy/3/main.cc
@@ -23,6 +23,8 @@ int main(int argc, char** argv){
 
   if(argc < 3){
     cout << "Not enough arguments." << endl;
+    cout << "Use as " << argv[0] << " [key] [message]" << std::endl;
+    cout << "[key] is interpretted as a number, and [message] as text" << std::endl;
     return 1;
   }
 
