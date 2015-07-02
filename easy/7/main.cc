f84f1ce90f582efebae36bd96e29c06b4572043a
Chris Samuelson <chris.sam55@gmail.com>
2015-07-01 21:50:55 -0400

    Added morse definition file and translation code.

--- easy/7/main.cc
+++ easy/7/main.cc
@@ -1,8 +1,29 @@
 #include<string>
 #include<fstream>
 #include<iostream>
+#include<map>
 
 int main(){
+  std::ifstream mFile("morse");
+  std::ifstream file("text");
+  std::string line;
+  std::string output;
+  std::map<std::string, std::string> words;
+
+  words[std::string("/")] = std::string(" ");
+  while(mFile >> line){
+    std::string letter;
+    mFile >> letter;
+    words[line] = letter;
+  }
+
+  while(file >> line){
+    output += words[line];
+  }
+
+  std::cout << output << std::endl;
+
+  file.close();
   return 0;
 }
 
