07d83a6eec3703b49b1a7a7bd5f5c72c842597f1
Chris Samuelson <chris.sam55@gmail.com>
2015-07-01 22:23:49 -0400

    Implemented relation checker.

--- hard/4/main.cc
+++ hard/4/main.cc
@@ -1,35 +1,29 @@
 #include<iostream>
 #include<string>
 #include<vector>
+#include<fstream>
 
-using std::cout;
-using std::endl;
-using std::string;
-using std::vector;
-
-int main(int argc, char** argv){
-/*
- *5, 3, 15
- *4, 2, 8
- *6, 2, 12
- */
-  string input = "5, 13, 15";
-  string delims = ", ";
-  vector<string> toks;
-  int offset = 0;
-
-  cout << input << endl;
-  //Find the tokens from the source string
-  while(offset < input.size()){
-    string tmp = input.substr(offset, input.find(delims, offset) - offset);
-    toks.push_back(tmp);
-    offset += tmp.size() + delims.size();
+int main(){
+  std::ifstream file("values");
+  std::vector<unsigned int> vals;
+  unsigned int val;
+  while(file >> val){
+    vals.push_back(val);
   }
 
-  for(int i = 0; i < toks.size(); ++i){
-    for(int j = 0; i < toks.size(); ++j){
-      if(j != i){
-      }
+  while(vals.size() >= 3){
+    unsigned int i = vals.back();
+    vals.pop_back();
+    unsigned int j = vals.back();
+    vals.pop_back();
+    unsigned int k = vals.back();
+    vals.pop_back();
+    if(i * j == k){
+      std::cout << i << " * " << j << " == " << k << std::endl;
+    } else if(i * k == j){
+      std::cout << i << " * " << k << " == " << j << std::endl;
+    } else if(j * k == i){
+      std::cout << j << " * " << k << " == " << i << std::endl;
     }
   }
 
