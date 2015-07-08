7053ac8b178e8cf96f1a534ae7b43d1767ba6521
Chris Samuelson <chris.sam55@gmail.com>
2015-07-08 18:57:53 -0400

    Implemented triangle calculation.

--- hard/8/main.cc
+++ hard/8/main.cc
@@ -1,13 +1,34 @@
 #include<iostream>
+#include<vector>
 
 int main(){
   unsigned int row;
   unsigned int idx;
+  std::vector<unsigned int> triangle;
+
   do{
     std::cout << "Input coordinates: ";
     std::cin >> row;
     std::cin >> idx;
   }while(idx > row);
+
+  triangle.push_back(1);
+  triangle.push_back(1);
+
+  for(unsigned int i = 2; i < row; ++i){
+    std::vector<unsigned int> tri;
+    auto first = triangle.begin();
+    auto second = ++triangle.begin();
+
+    tri.push_back(1);
+    for(auto it = triangle.begin(); it != triangle.end(); ++it){
+      tri.push_back((*first++) + (*second++));
+    }
+
+    triangle = tri;
+  }
+
+  std::cout << triangle[idx - 1] << std::endl;
 
   return 0;
 }
