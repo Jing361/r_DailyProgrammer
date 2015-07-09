e6a67081169adbb6026bff7e8602b867a876ca22
Chris Samuelson <chris.sam55@gmail.com>
2015-07-08 21:56:43 -0400

    Normalized format.

--- inter/4/calculator.cc
+++ inter/4/calculator.cc
@@ -1,27 +1,29 @@
 #include"calculator.hh"
 
-calculator::calculator():acc(0){
+calculator::calculator():
+  acc(0){
   for(int i = (int)'0'; i <= (int)'9'; ++i){
-    this->precedence.insert(std::pair<char, int>((char)i, 0));
+    precedence[(char)i] = 0;
   }
-  this->precedence.insert(std::pair<char, int>('*', 2));
-  this->precedence.insert(std::pair<char, int>('/', 2));
-  this->precedence.insert(std::pair<char, int>('+', 1));
-  this->precedence.insert(std::pair<char, int>('-', 1));
+  precedence['*'] = 2;
+  precedence['/'] = 2;
+  precedence['+'] = 1;
+  precedence['-'] = 1;
 }
 
-calculator::calculator(std::string str):calculator(){
-  this->parse(str);
+calculator::calculator(std::string str):
+  calculator(){
+  parse(str);
 }
 
 int calculator::evaluate(){
   std::stack<char> eval;
 
-  while(!this->rpn.empty()){
-    if(precedence[this->rpn.back()] == 0){
-      eval.push(this->rpn.back());
-      this->rpn.erase(this->rpn.size());
-    }else if(precedence[this->rpn.back()] > 0){
+  while(!rpn.empty()){
+    if(precedence[rpn.back()] == 0){
+      eval.push(rpn.back());
+      rpn.erase(rpn.size());
+    }else if(precedence[rpn.back()] > 0){
       if(eval.size() < 2){
         return -1;
       }
@@ -30,13 +32,13 @@ int calculator::evaluate(){
       eval.pop();
       op2 = ((int)eval.top() - (int)'0');
       eval.pop();
-      if(this->rpn.back() == '*'){
+      if(rpn.back() == '*'){
         res = op1 * op2;
-      } else if(this->rpn.back() == '/'){
+      } else if(rpn.back() == '/'){
         res = op1 / op2;
-      } else if(this->rpn.back() == '+'){
+      } else if(rpn.back() == '+'){
         res = op1 + op2;
-      } else if(this->rpn.back() == '-'){
+      } else if(rpn.back() == '-'){
         res = op1 - op2;
       }
       eval.push(res);
@@ -51,21 +53,21 @@ int calculator::evaluate(){
 
 void calculator::parse(std::string str){
   std::stack<char> ops;
-  this->rpn.clear();
+  rpn.clear();
 
   for(auto it = str.begin(); it != str.end(); ++it){
-    if(this->precedence[*it] == 0){
-      this->rpn += *it;
-    } else if(this->precedence[*it] > 0){
-      while(!ops.empty() && this->precedence[*it] <= this->rpn.back()){
-        this->rpn += ops.top();
+    if(precedence[*it] == 0){
+      rpn += *it;
+    } else if(precedence[*it] > 0){
+      while(!ops.empty() && precedence[*it] <= rpn.back()){
+        rpn += ops.top();
         ops.pop();
       }
       ops.push(*it);
     }
   }
   while(!ops.empty()){
-    this->rpn += ops.top();
+    rpn += ops.top();
     ops.pop();
   }
 }
--- inter/4/main.cc
+++ inter/4/main.cc
@@ -2,16 +2,11 @@
 #include<string>
 #include"calculator.hh"
 
-using std::cout;
-using std::endl;
-using std::cin;
-using std::string;
-
 int main(int argc, char** argv){
-  string start = "3+4*2/4";
+  std::string start = "3+4*2/4";
   calculator calc(start);
 
-  cout << calc.evaluate() << endl;
+  std::cout << calc.evaluate() << std::endl;
 
   return 0;
 }
