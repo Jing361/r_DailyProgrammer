f7e219548481e701ca3e83d057fb12a07fbee3da
Chris Samuelson <chris.sam55@gmail.com>
2015-07-10 11:21:14 -0400

    Added type checking.

--- easy/1/main.cc
+++ easy/1/main.cc
@@ -1,24 +1,36 @@
 #include<iostream>
 #include<fstream>
 #include<string>
+#include<limits>
+#include<sstream>
 
 //TODO: Add input type checking.
-int main(int argc, char **argv){
+int main(){
   std::string name;
-  std::string age;
+  unsigned int age;
   std::string username;
   std::ofstream oFile("record");
 
   std::cout << "Name: ";
   std::cin >> name;
-  std::cout << "Age: ";
-  std::cin >> age;
+  while(std::cout << "Age: " && !(std::cin >> age)){
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cout << "Not a number" << std::endl;
+  }
   std::cout << "Username: ";
   std::cin >> username;
 
-  std::cout << "Your name is " << name << " you are " << age << " years old, your username is " << username << "." << std::endl;
-  oFile << "Your name is " << name << " you are " << age << " years old, your username is " << username << "." << std::endl;
-  oFile.close();
+  std::stringstream ss;
+  std::string sAge;
+  ss << age;
+  ss >> sAge;
+  
+  std::string output = "Your name is " + name + " you are " + sAge + " years old, your username is " + username + ".\n";
 
+  std::cout << output;
+  oFile << output;
+
+  oFile.close();
   return 0;
 }
