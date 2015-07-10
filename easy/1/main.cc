870f94b09d7f681e33394deee72e33d0626bb695
Chris Samuelson <chris.sam55@gmail.com>
2015-07-10 10:52:04 -0400

    Standardized qualifiers.

--- easy/1/main.cc
+++ easy/1/main.cc
@@ -1,28 +1,23 @@
 #include<iostream>
 #include<fstream>
 
-using std::cout;
-using std::endl;
-using std::cin;
-using std::ofstream;
-
 //TODO: Add input type checking.
 int main(int argc, char **argv){
     char name[255];
     char age[5];
     char username[255];
-    ofstream oFile;
+    std::ofstream oFile;
     oFile.open("record");
 
-    cout << "Name: ";
-    cin >> name;
-    cout << "Age: ";
-    cin >> age;
-    cout << "Username: ";
-    cin >> username;
+    std::cout << "Name: ";
+    std::cin >> name;
+    std::cout << "Age: ";
+    std::cin >> age;
+    std::cout << "Username: ";
+    std::cin >> username;
 
-    cout << "Your name is " << name << " you are " << age << " years old, your username is " << username << "." << endl;
-    oFile << "Your name is " << name << " you are " << age << " years old, your username is " << username << "." << endl;
+    std::cout << "Your name is " << name << " you are " << age << " years old, your username is " << username << "." << std::endl;
+    oFile << "Your name is " << name << " you are " << age << " years old, your username is " << username << "." << std::endl;
     oFile.close();
 
     return 0;
