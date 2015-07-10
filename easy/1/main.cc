3a10c86ba79bd021b6782f92c3ce1312557eba88
Chris Samuelson <chris.sam55@gmail.com>
2015-07-10 10:54:08 -0400

    Fixed indenting, and switched to std::string.

--- easy/1/main.cc
+++ easy/1/main.cc
@@ -1,24 +1,24 @@
 #include<iostream>
 #include<fstream>
+#include<string>
 
 //TODO: Add input type checking.
 int main(int argc, char **argv){
-    char name[255];
-    char age[5];
-    char username[255];
-    std::ofstream oFile;
-    oFile.open("record");
+  std::string name;
+  std::string age;
+  std::string username;
+  std::ofstream oFile("record");
 
-    std::cout << "Name: ";
-    std::cin >> name;
-    std::cout << "Age: ";
-    std::cin >> age;
-    std::cout << "Username: ";
-    std::cin >> username;
+  std::cout << "Name: ";
+  std::cin >> name;
+  std::cout << "Age: ";
+  std::cin >> age;
+  std::cout << "Username: ";
+  std::cin >> username;
 
-    std::cout << "Your name is " << name << " you are " << age << " years old, your username is " << username << "." << std::endl;
-    oFile << "Your name is " << name << " you are " << age << " years old, your username is " << username << "." << std::endl;
-    oFile.close();
+  std::cout << "Your name is " << name << " you are " << age << " years old, your username is " << username << "." << std::endl;
+  oFile << "Your name is " << name << " you are " << age << " years old, your username is " << username << "." << std::endl;
+  oFile.close();
 
-    return 0;
+  return 0;
 }
