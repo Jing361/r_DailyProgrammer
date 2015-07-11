1082045c9b3112f9bc11762a33dcdc23f05919c5
Chris Samuelson <chris.sam55@gmail.com>
2015-07-10 20:14:58 -0400

    Added some new prompts.

---
+++ hard/0/6/main.cc
@@ -0,0 +1,12 @@
+
+int main(){
+  std::cout << "AI options are:\n";
+  std::cout << "\tBFS\n";
+  std::cout << "\tDFS\n";
+  std::cout << "\tA*\n";
+  std::cout << "\tIterative deepening\n";
+  std::cout << "\tminimax\n";
+  std::cout << "\tKnown win strat\n";
+  return 0;
+}
+
