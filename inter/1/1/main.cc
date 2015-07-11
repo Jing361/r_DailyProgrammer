6c936c5ee63f7cbc9d3b86094ae2d9d4e17e19c4
Chris Samuelson <chris.sam55@gmail.com>
2015-07-10 23:31:50 -0400

    Switched to initializer list.

--- inter/1/1/main.cc
+++ inter/1/1/main.cc
@@ -2,14 +2,14 @@
 #include<map>
 
 int main(){
-  std::map<unsigned int, unsigned int> upside;
-  upside.insert(std::pair<unsigned int, unsigned int>(1, 1));
-  upside.insert(std::pair<unsigned int, unsigned int>(2, 2));
-  upside.insert(std::pair<unsigned int, unsigned int>(5, 5));
-  upside.insert(std::pair<unsigned int, unsigned int>(6, 9));
-  upside.insert(std::pair<unsigned int, unsigned int>(8, 8));
-  upside.insert(std::pair<unsigned int, unsigned int>(9, 6));
-  upside.insert(std::pair<unsigned int, unsigned int>(0, 0));
+  std::map<unsigned int, unsigned int> upside{ 
+    {1, 1},
+    {2, 2},
+    {5, 5},
+    {6, 9},
+    {8, 8},
+    {9, 6},
+    {0, 0} };
 
   return 0;
 }
