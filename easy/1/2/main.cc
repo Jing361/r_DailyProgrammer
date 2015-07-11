d0228f05fcdaddfdcbe5bb42f7f06164523008e8
Chris Samuelson <chris.sam55@gmail.com>
2015-07-10 22:30:32 -0400

    Implemented basic solution.

--- easy/1/2/main.cc
+++ easy/1/2/main.cc
@@ -5,15 +5,34 @@
 
 #include<set>
 
-void bfs(std::vector<std::string>& words, std::string state, std::string source){
-  std::stack<std::pair<std::string, std::string>> frontier;
-  std::vector<std::pair<std::string, std::string>> explored;
+std::vector<std::string> generateSuccessors(std::string state, std::string source){
+  std::vector<std::string> ret;
+  for(auto it = source.begin(); it != source.end(); ++it){
+    ret.push_back(state + *it);
+  }
+  return ret;
+}
 
-  frontier.push(std::pair<std::string, std::string>(state, source));
+void bfs(std::vector<std::string>& words, std::string source){
+  std::stack<std::string> frontier;
+  std::set<std::string> explored;
+
+  frontier.push(std::string(""));
 
   while(!frontier.empty()){
-    auto thisnode = frontier.pop();
-    explored.push_back();
+    auto thisnode = frontier.top();
+    frontier.pop();
+    explored.insert(thisnode);
+    if(thisnode.length() == source.length()){
+      words.push_back(thisnode);
+    }
+    auto suc = generateSuccessors(thisnode, source);
+    for(auto it = suc.begin(); it != suc.end(); ++it){
+      if((explored.count(*it) > 0) || ((*it).length() > source.length())){
+        continue;
+      }
+      frontier.push(*it);
+    }
   }
 }
 
@@ -21,9 +40,10 @@ int main(){
   std::string str("hi!");
   std::vector<std::string> words;
 
-  bfs(words, str, std::string(""));
+  bfs(words, str);
 
   for(auto it = words.begin(); it != words.end(); ++it){
+    std::cout << *it << std::endl;
   }
 
   return 0;
