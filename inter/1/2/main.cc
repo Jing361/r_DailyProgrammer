4ccd228154332e221ba7dbb209955b7b0853be38
Chris Samuelson <chris.sam55@gmail.com>
2015-11-08 18:35:07 -0500

    Implemented factoring algorithm.

---
+++ inter/1/2/main.cc
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+unsigned int factor(unsigned int num){
+  unsigned int i = 2;
+
+  while(i < num){
+    if(num % i == 0){
+      break;
+    }
+    ++i;
+  }
+
+  if(i == num){
+    i = 1;
+  }
+
+  return i;
+}
+
+int main(){
+  unsigned int num = 12;
+  std::vector<unsigned int> factors;
+
+  while(num != 1){
+    unsigned int next = factor(num);
+    factors.push_back(next);
+    if(next == 1){
+      break;
+    } else {
+      num /= next;
+    }
+  }
+
+  for_each(factors.begin(), factors.end(), [](unsigned int x){
+    std::cout << x << std::endl;
+  });
+
+  return 0;
+}
+
