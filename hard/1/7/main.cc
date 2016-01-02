fecb370aaca21ddb8bdf3dfa8860aefd7b0257de
Chris Samuelson <chris.sam55@gmail.com>
2016-01-02 17:10:06 -0500

    Changed players to be stored in a list, and accounted for memory mgmt.

--- hard/1/7/main.cc
+++ hard/1/7/main.cc
@@ -1,17 +1,20 @@
 #include<iostream>
+#include<vector>
 #include"board.hh"
 #include"player.hh"
 
 int main(){
   board game;
-  human one(Ecks);
-  ai two(Oh);
+  std::vector<player*> players = { new human(Ecks), new ai(Oh) };
 
   while(!game){
     game.render();
     std::cout << "beep boop" << std::endl;
   }
 
+  for(auto it:players){
+    delete it;
+  }
   return 0;
 }
 
