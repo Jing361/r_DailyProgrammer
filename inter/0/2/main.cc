409798b92dcfedf67a1c7b002633b98a0201d28f
Chris Samuelson <chris.sam55@gmail.com>
2016-01-26 22:02:08 -0500

    Allowed cli input of file name.

--- inter/0/2/main.cc
+++ inter/0/2/main.cc
@@ -1,8 +1,11 @@
 #include"AdventureGame.hh"
 
-int main(){
-  Game& game = *new AdventureGame("moon.adv");
-  game.run();
+int main(int argc, char** argv){
+  Game* game;
+  if(argc == 2){
+    game = new AdventureGame(argv[1]);
+  }
+  game->run();
   return 0;
 }
 
