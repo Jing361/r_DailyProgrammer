2cc32c7614341606af911d78d0757f390afe2f32
Chris Samuelson <chris.sam55@gmail.com>
2016-08-16 09:55:22 -0400

    Made actual genius list

--- hard/2/1/source/main.cc
+++ hard/2/1/source/main.cc
@@ -1,9 +1,12 @@
 #include<iostream>
+#include<vector>
 #include<fstream>
 #include<set>
+#include<map>
 #include<string>
 #include<sstream>
 #include<exception>
+#include<tuple>
 
 using namespace std;
 
@@ -32,25 +35,38 @@ int main(int argc, char** argv){
   }
 
   set<string> ppl;
+  vector<string> list;
+  multimap<string, string> relations;
   fstream fs(argv[1]);
 
   string line;
   while(getline(fs, line)){
     stringstream ss(line);
     string tok;
+    string one;
+    string two;
 
     ss >> tok;
+    if(!ppl.count(tok)){
+      list.push_back(tok);
+    }
+    one = tok;
     ppl.insert(tok);
+
     ss >> tok;
     if(tok.size() != 1 && tok[0] != ':'){
       throw formatFailureException("File: " + string(argv[1]) + " is incorrectly formatted.");
     }
-    ss >> tok;
-    ppl.insert(tok);
-  }
 
-  for(auto it:ppl)
-    cout << it << endl;
+    ss >> tok;
+    if(!ppl.count(tok)){
+      list.push_back(tok);
+    }
+    two = tok;
+    ppl.insert(tok);
+
+    relations.insert(make_pair(one, two));
+  }
 
   return 0;
 }
