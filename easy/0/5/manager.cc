1d9bb0c03c1740bae09d7b8cc44015f67285515d
Chris Samuelson <chris.sam55@gmail.com>
2017-01-27 15:45:10 -0500

    Formatting updates

--- easy/0/5/manager.cc
+++ easy/0/5/manager.cc
@@ -8,31 +8,31 @@
 using namespace std;
 
 int main(){
-  authenticator auth(string("user"));
+  authenticator auth( string( "user" ) );
   string user;
 
-  for(auto it = auth.users.begin(); it != auth.users.end(); ++it){
-    cout << get<0>(*it) << endl;
-    cout << get<0>(get<1>(*it)) << endl;
-    cout << get<1>(get<1>(*it)) << endl;
+  for( auto it = auth.users.begin(); it != auth.users.end(); ++it ){
+    cout << get<0>( *it ) << endl;
+    cout << get<0>( get<1>( *it ) ) << endl;
+    cout << get<1>( get<1>( *it ) ) << endl;
     cout << endl;
   }
 
-  int opt;
-  while(true){
-    while(cout << "What would you like to do?\t\n"
-                    << "1.\tAdd a user\n"
-                    << "2.\tRemove a user\n"
-                    << "3.\tFinish\n" &&
-        (!(cin >> opt) || opt < 1 || opt > 3)){
+  while( true ){
+    int opt;
+    while( cout << "What would you like to do?\t\n"
+                << "1.\tAdd a user\n"
+                << "2.\tRemove a user\n"
+                << "3.\tFinish\n" &&
+           ( !( cin >> opt ) || opt < 1 || opt > 3 ) ){
       cin.clear(); 
-      cin.ignore(numeric_limits<streamsize>::max(), '\n');
+      cin.ignore( numeric_limits<streamsize>::max(), '\n' );
       cout << "Invalid input\n";
     }
     string name;
     string pass;
 
-    if(opt < 3){
+    if( opt < 3 ){
       cout << "Please enter username and password for user:" << endl;
       cout << "username:\t";
       cin >> name;
@@ -42,11 +42,11 @@ int main(){
         cin >> pass;
       }
     }
-    if(opt == 1){
-      auth.mkUser(name, pass);
-    } else if(opt == 2){
-      auth.rmUser(name, pass);
-    } else if(opt == 3){
+    if( opt == 1 ){
+      auth.mkUser( name, pass );
+    } else if( opt == 2 ){
+      auth.rmUser( name, pass );
+    } else if( opt == 3 ){
       cout << "Quitting" << endl;
       auth.writeUsers();
       break;
