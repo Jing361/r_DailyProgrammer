a2bc632b8c55069bec59005e66b69bf4ad3f9ec7
Chris Samuelson <chris.sam55@gmail.com>
2017-01-30 15:41:16 -0500

    Improved best practice usage

--- easy/0/5/main.cc
+++ easy/0/5/main.cc
@@ -2,6 +2,7 @@
 #include<vector>
 #include<string>
 #include<fstream>
+#include<limits>
 
 #include"authenticator.hh"
 #include"hide_std_in.hh"
@@ -9,25 +10,35 @@
 using namespace std;
 
 int main(){
-  authenticator auth(string("user"));
+  fstream testout( "testout", ios::app );
+
+  authenticator auth( string( "user" ) );
   string username;
   string password;
 
   cout << "username:\t";
   while( !( cin >> username ) ){
+    cin.clear();
+    cin.ignore( numeric_limits<streamsize>::max(), '\n' );
     cout << "format error!" << endl;
   }
+
+testout << 'A' << username << 'A' << endl;
+  cout << '\n';
 
   {
     hide_std_in hide;
 
     cout << "password:\t";
     while( !( cin >> password ) ){
-      cerr << "format error!" << endl;
+      cin.clear();
+      cin.ignore( numeric_limits<streamsize>::max(), '\n' );
+      cout << "format error!" << endl;
     }
   }
 
-  cout << endl;
+testout << 'A' << password << 'A' << endl;
+  cout << '\n';
 
   if( !auth.authenticate( username, password ) ){
     cout << "you lose!" << endl;
